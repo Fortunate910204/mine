c0dda26d00b3bb3608e5f9d645316ccde2f373d5
张渊杰 <zhangyuanjie@1jiajie.com>
2016-04-13 11:06:40 +0800

    结构

---
+++ ImprMySkil/SupportingFiles/BridgeHeader.h
@@ -0,0 +1,13 @@
+//
+//  BridgeHeader.h
+//  ImprMySkil
+//
+//  Created by Ejiajie on 16/4/12.
+//  Copyright © 2016年 Ejiajie. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BridgeHeader : NSObject
+
+@end
---
+++ ImprMySkil/SupportingFiles/BridgeHeader.m
@@ -0,0 +1,13 @@
+//
+//  BridgeHeader.m
+//  ImprMySkil
+//
+//  Created by Ejiajie on 16/4/12.
+//  Copyright © 2016年 Ejiajie. All rights reserved.
+//
+
+#import "BridgeHeader.h"
+
+@implementation BridgeHeader
+
+@end
